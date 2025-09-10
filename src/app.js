018f305af1f9afc78e478b81a0aaf03139ec40e0
Zulu <juanpablozuluaga.0311@gmail.com>
2025-09-10 09:50:50 -0500

    Política de CORS

--- src/app.js
+++ src/app.js
@@ -6,7 +6,9 @@ import cors from 'cors';
 const app = express();
 
 app.use(express.json());
-app.use(cors()); 
+app.use(cors({
+      origin: "https://www.devcorebits.com"
+})); 
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/health", (req, res) => res.send("OK"));
